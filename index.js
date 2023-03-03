e55fea3f8d0c7a546e685fd1883d6be709d22334
Matheus Ortiz <math.ortiz@outlook.com>
2023-03-03 19:21:05 -0300

    #burguer

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "./imagens/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "./imagens/close_white_36dp.svg";
-    }
-}
+const burguer  = document.querySelector(".burguer");
+const navMenu = document.querySelector(".nav-list");
+
+burguer.addEventListener("click", () => {
+    burguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
