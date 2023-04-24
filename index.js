aef0aa030e8bd4276c9bd89664b34ee7da273c18
Matheus Ortiz <math.ortiz@outlook.com>
2023-04-24 18:03:11 -0300

    a little bit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const burguer  = document.querySelector(".burguer");
 const navMenu = document.querySelector(".nav-list");
+const navItem = document.querySelector(".nav-item");
 
 burguer.addEventListener("click", () => {
     burguer.classList.toggle('active');
     navMenu.classList.toggle('active');
+    navItem.classList.toggle('active');
 })
